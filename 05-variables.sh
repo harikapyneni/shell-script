1b825d005aa9801143183ffe7c4c23ed79ec1341
harikapyneni <snehapyneni@gmail.com>
2023-08-19 19:54:09 -0400

    username&password

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# user and password
+# user and password , this is only for practice 
 
 echo "Please Enter Your UserName "
 read USERNAME
 echo "UserName You Entered is :$USERNAME"
+
+#In Realtime you cant see password in execution
 
 echo "Please Enter Your Password "
 read -s PASSWORD
