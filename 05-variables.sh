35af1c74c62b2a0179b8061993bde6043aed5da5
harikapyneni <snehapyneni@gmail.com>
2023-08-19 19:48:06 -0400

    username&password

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# user and password
+
+echo "Please Enter Your UserName "
+read USERNAME
+echo "UserName You Entered is :$USERNAME"
+
+echo "Please Enter Your Password "
+read -s PASSWORD
+echo "Password You Entered is :$PASSWORD"
