bf43bb79a08a9a1a4d1244d47cbb8a4edceebb4f
harikapyneni <snehapyneni@gmail.com>
2023-08-19 18:32:53 -0400

    chatting between two persons

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+PERSON1=$1 #this is the first argument from command line
+PERSON2=$2 #this is the second argument from command line
+
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hello $PERSON1, how are you?"
+echo "$PERSON1 : Good , Thanks for asking.$PERSON2 "
