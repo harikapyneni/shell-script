45f25f2bc061b19bc00e6e971b13968496655da3
harikapyneni <snehapyneni@gmail.com>
2023-08-21 17:30:57 -0400

    adding colors to existing

--- 06-array.sh
+++ 06-array.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
+R="\e[31m"
+G="\e[32m"
 PERSONS=("EDDIE" "JOHN" "ROBERT")
-echo "First Person:${PERSONS[0]}"
+
+echo "First Person:$R ${PERSONS[0]}"
+echo "all Persons :$G ${PERSONS[@]}"
