31e9ac51b30d4378b70cb97be06ea667520ca9b3
harikapyneni <snehapyneni@gmail.com>
2023-08-21 19:43:19 -0400

    loop

---
+++ 13-loops.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#echo 1
+#echo 2
+#echo 3
+#echo 4
+
+# we cant write 100's of code for printing 100 numbers for example,so we use 'for' loop
+
+for i in {1..100}
+do
+    echo $i
+done
