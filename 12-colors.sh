df0dbae477b0810ac2cd068588a454b2265634a5
harikapyneni <snehapyneni@gmail.com>
2023-08-21 18:50:57 -0400

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -9,5 +9,5 @@ STUDENT2="anusha"
 STUDENT3="apple"
 STUDENT4="dayansh"
 
-echo -e "My Student is : $R $STUDENT2 "
+   echo -e "My Student is : $R $STUDENT2 "
 echo -e "$N My Fav Student is: $V $STUDENT3 $STUDENT4 "
