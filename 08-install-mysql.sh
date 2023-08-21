3fba16891481858a49671250756815736e2bff39
harikapyneni <snehapyneni@gmail.com>
2023-08-20 22:46:42 -0400

    changes

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -5,16 +5,19 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-
 then
-
     echo "ERROR! please run this script with Root access of user"
-
     exit 1
-
 #else
 #echo "INFO : you're a root user"
-
 fi 
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql is ERROR!"
+    exit 1
+else
+    echo "installation of mysql is SUCCESS"
+fi
