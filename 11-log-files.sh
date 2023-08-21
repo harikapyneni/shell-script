e9764fa8abc5a373045adb8b61c2f8a3f8520d77
harikapyneni <snehapyneni@gmail.com>
2023-08-21 16:20:27 -0400

    logfiles with function prog

--- 11-log-files.sh
+++ 11-log-files.sh
@@ -3,4 +3,34 @@
 DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/SCRIPT_NAME-DATE.log
+USERID=$(id -u)
 
+# $1 --> it will recieve the first argument
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}  
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: Please run this script with root access"
+    exit 1
+#else 
+#   echo "INFO :: You are a root user"
+fi
+
+# It is your responsibility to check the installation is success or not 
+
+yum install postfix -y
+
+VALIDATE $? "Installing postfix..."
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql..."
