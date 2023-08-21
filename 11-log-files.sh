ab402531246ea7b7e6f5bb3061c7b4696b9e8c8a
harikapyneni <snehapyneni@gmail.com>
2023-08-21 16:28:00 -0400

    logfiles with function prog without lengthy output

--- 11-log-files.sh
+++ 11-log-files.sh
@@ -29,10 +29,10 @@ fi
 
 # It is your responsibility to check the installation is success or not 
 
-yum install postfix -y &>>LOGFILE
+yum install postfix -y &>>$LOGFILE
 
 VALIDATE $? "Installing postfix..."
 
-yum install mysql -y &>>LOGFILE
+yum install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installing mysql..."
