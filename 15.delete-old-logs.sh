9f5b416dae4083a3a2ebcd3ae7f4f7bc5477fa1c
harikapyneni <snehapyneni@gmail.com>
2023-08-22 16:33:42 -0400

    old logs

---
+++ 15.delete-old-logs.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+APPS_LOGSDIR=/home/centos/apps-logs
+
+DATE=$(date +%F:%H:%M:%S)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+FILES_TO_DELETE=$(find $APP_LOGSDIR "*.log" -type f -mtime +14)
+
+echo "Scripts started executing at $DATE" &>>$LOGSDIR
+
+while read line
+do
+    echo "Deleting $LINE" &>>$LOGSDIR
+    rm -rf $LINE
+done <<< $FILES_TO_DELETE
