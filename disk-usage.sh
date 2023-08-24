3bbf94710123f9bee482beb7fa2b0533e9fbb3f0
harikapyneni <snehapyneni@gmail.com>
2023-08-24 17:22:59 -0400

    disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -25,7 +25,7 @@ do
     partition=$(echo $line | awk '{print $1}')
 
     #now you need to check whether usage is more than threshold or not
-    if [ $USAGE -gt $DISK_USAGE_THRESHOLD ];
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
         message+="HIGH DISK USAGE on $partition: $usage"
     fi
