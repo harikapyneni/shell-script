b0055ca08ad8c9b4f35bd425a5d232d4fa5650c6
harikapyneni <snehapyneni@gmail.com>
2023-08-24 18:37:26 -0400

    disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -25,11 +25,11 @@ do
     #now you need to check whether usage is more than threshold or not
     if [ $USAGE -gt $DISK_USAGE_THRESHOLD ];
     then
-        message+="HIGH DISK USAGE on $PARTITION: \n$USAGE"
+        message+="HIGH DISK USAGE on $PARTITION: $USAGE\n"
     fi
 done <<< $DISK_USAGE
 
-echo -e "message: $MESSAGE"
+echo -e "message: $message"
 
 #echo "$message | mail -s "High Disk Usage" info@joindevops.com
 #how to call other shellscript from your current script - by using sh 
