32b48680f94c7dd6238df7a4419936b330192954
harikapyneni <snehapyneni@gmail.com>
2023-08-24 19:51:10 -0400

    disk usage

--- mail.sh
+++ mail.sh
@@ -15,8 +15,5 @@ ALERT_TYPE=$5
 #echo "$BODY" | mail -s "High Disk Usage example@email.com"
 #construct a proper template- (template.html) and using sed editor to substitute the above arguments with this command
 
-FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/" template.html)
-
-#echo "$FINAL_BODY" | mail -s "$SUBJECT" content is in html format to send gmail "$TO_ADDRESS"
-
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
