2ebe160567b9b0847177bf0ad957437ba8515b70
harikapyneni <snehapyneni@gmail.com>
2023-08-24 15:16:36 -0400

    disk usage

--- mail.sh
+++ mail.sh
@@ -11,6 +11,10 @@ TEAM_NAME=$4
 ALERT_TYPE=$5
 
 #echo "all Args: $@"
-#echo "$Message | mail -s "High Disk Usage info@joindevops.com"
+
+#echo "$Message | mail -s "High Disk Usage example@email.com"
+#construct a proper template- (template.html) and using sed editor to substitute the above arguments with this command
+
+FINAL_BODY=$(sed -e 's/TEAM_NAME/Devops/g' -e 's/ALERT_TYPE/High Disk Usage/g' -e "s/MESSAGE/$BODY/g" template.html)
 
 echo "$BODY" | mail -s "$SUBJECT" $TO_ADDRESS
